cc209a2784d601c1ffcc711c4e2917540371c89e
Dipo Arowona <abdulsalam.arowona@gmail.com>
2020-09-11 16:41:25 -0400

    including data for countries

--- covid-data.py
+++ covid-data.py
@@ -38,41 +38,88 @@ def get_timeStamp():
 #check to see if date in last database entry is today 
 #else make a new entry 
 
-client = pymongo.MongoClient(Database_URI)
-db = client["Covid"]
-total_col = db["total_data"]
 
-last_item = total_col.find().sort("_id",-1).limit(1) #returns a list with only one value
+def insert_total_covid_data():
+        
+    client = pymongo.MongoClient(Database_URI)
+    db = client["Covid"]
+    total_col = db["total_data"]
 
-if last_item[0]["date"] == get_timeStamp().strftime("%m/%d/%Y"):
-    total_data = get_totals()
-    date_query = {"date":get_timeStamp().strftime("%m/%d/%Y")}
+    last_item = total_col.find().sort("_id",-1).limit(1) #returns a list with only one value
+
+    if last_item[0]["date"] == get_timeStamp().strftime("%m/%d/%Y"):
+        total_data = get_totals()
+        date_query = {"date":get_timeStamp().strftime("%m/%d/%Y")}
+        
+        total_col.update_one(date_query, { "$set": {
+            "timestamp":get_timeStamp().strftime("%m/%d/%Y - %H:%M:%S.%f"), 
+            "date":get_timeStamp().strftime("%m/%d/%Y"),
+            "total_cases":total_data[4],
+            'recovered_cases':total_data[1],
+            'deaths':total_data[0],
+            'critical_cases':total_data[2],
+            'active_cases':total_data[3]
+        }})
+
+        print("info updated")
+
+        #update the total info 
+    else:
+        total_data = get_totals()
+
+        total_col.insert_one({
+            "timestamp":get_timeStamp().strftime("%m/%d/%Y - %H:%M:%S.%f"), 
+            "date":get_timeStamp().strftime("%m/%d/%Y"),
+            "total_cases":total_data[4],
+            'recovered_cases':total_data[1],
+            'deaths':total_data[0],
+            'critical_cases':total_data[2],
+            'active_cases':total_data[3]
+        })
+        print("new entry made")
+        #make new entry info
+
     
-    total_col.update_one(date_query, { "$set": {
-        "timestamp":get_timeStamp().strftime("%m/%d/%Y - %H:%M:%S.%f"), 
-        "date":get_timeStamp().strftime("%m/%d/%Y"),
-        "total_cases":total_data[4],
-        'recovered_cases':total_data[1],
-        'deaths':total_data[0],
-        'critical_cases':total_data[2],
-        'active_cases':total_data[3]
-    }})
+def insert_country_covid_data():
+    client = pymongo.MongoClient(Database_URI)
+    db = client["Covid"]
+    country_col = db["country_data"]
+    
+    date = get_timeStamp().strftime("%Y-%m-%d")
+    countries = ["Switzerland", "Canada", "Japan", "Germany", "Australia", "United Kingdom", "United States", "India", "Brazil", "Russia", "Mexico" ]
+    
+    #get data for each country     
+    #insert new data for each country
+    for country in countries:
+        #if country has an entry add 
+        #else create a new one and add
 
-    print("info updated")
 
-    #update the total info 
-else:
-    total_data = get_totals()
 
-    total_col.insert_one({
-        "timestamp":get_timeStamp().strftime("%m/%d/%Y - %H:%M:%S.%f"), 
-        "date":get_timeStamp().strftime("%m/%d/%Y"),
-        "total_cases":total_data[4],
-        'recovered_cases':total_data[1],
-        'deaths':total_data[0],
-        'critical_cases':total_data[2],
-        'active_cases':total_data[3]
-    })
-    print("new entry made")
-    #make new entry info
+
+    
+
+def get_country_data(country,date):
+    url_country = "https://covid-19-data.p.rapidapi.com/report/country/name"
+    querystring = {"date-format":"YYYY-MM-DD","format":"json","date":date,"name":country}
+    headers = {
+        'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
+        'x-rapidapi-key': "491f353b91msh4d6e560099cab14p16dc45jsn2ebbe77bc738"
+        }
+
+    response = requests.request("GET", url_country, headers=headers, params=querystring)
+
+    json_response = response.json()[0]["provinces"][0]
+
+    country_cases = json_response["confirmed"]
+    country_recovered = json_response["recovered"]
+    country_deaths = json_response["deaths"]
+    country_active = json_response["confirmed"] - (country_recovered + country_deaths)
+    
+    return (country_cases, country_deaths, country_active)
 #country data
+
+
+#run funtions
+
+
